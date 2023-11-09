ad46b00a31b85568c085403c48f3b86efc8817a5
patrictobola <tobola92@gmail.com>
2023-11-09 17:07:16 +0100

    fatto l'esercizio lasciando indietro il bonus

--- src/org/lessons/java/array/CibiPreferiti.java
+++ src/org/lessons/java/array/CibiPreferiti.java
@@ -1,5 +1,20 @@
 package org.lessons.java.array;
 
 public class CibiPreferiti {
-
+	public static void main(String[] args) {
+		
+//	Creo un nuovo array contenente una lista di cibi 
+	String[] favouriteFood = {"Sushi", "Pizza", "Pasta", "Insalata", "BBQ", "Gelato"};
+	
+//	Stampo a video la lunghezza della classifica
+	System.out.println(favouriteFood.length);
+	
+//	Stampo a video il primo in classifica
+	System.out.println(favouriteFood[0]);
+	
+//	Stampo a video l'ultimo in classifica
+	System.out.println(favouriteFood[favouriteFood.length - 1]);
+	
+	
+	}
 }
